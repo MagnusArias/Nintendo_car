2336c93e45b30900913ab919c724c84372fc2931
Przemyslaw Debiec <194933@edu.p.lodz.pl>
2016-12-18 20:01:31 +0100

    Dodano bazowe pliki Makefile oraz main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#define F_CPU 16000000UL
+#include <avr/io.h> 
+#include <util/delay.h>  
+ 
+#define FOSC F_CPU
+#define BAUD 115200
+
+int main(void)  
+{  	
+	
+	while(1)
+	{
+		
+	}
+	
+	return 0; 
+} 
+
