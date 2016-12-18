318e9278bd557869a3c28bdf175dd0d450611f8b
Przemyslaw Debiec <194933@edu.p.lodz.pl>
2016-12-18 20:08:31 +0100

    Pierwsze uzycie biblioteki ekranu

--- main.c
+++ main.c
@@ -2,15 +2,18 @@
 #include <avr/io.h> 
 #include <util/delay.h>  
  
+#include "HD44780.h"
 #define FOSC F_CPU
 #define BAUD 115200
 
 int main(void)  
 {  	
-	
+	LCD_Initalize();
 	while(1)
-	{
-		
+	{	
+		LCD_Clear();
+		LCD_GoTo(0,0);
+		LCD_WriteText("Hello!");
 	}
 	
 	return 0; 
