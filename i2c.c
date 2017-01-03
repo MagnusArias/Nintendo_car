0eb84d0efe36934f1a713b97278901f0dd2de73d
Przemyslaw Debiec <194933@edu.p.lodz.pl>
2017-01-03 19:54:22 +0100

    zmieniono CALKOWICIE biblioteke do obslugi I2C, poprzednia nie spelniala swojego zadania, dodano spis rejestrow z modulu LSM303D

--- i2c.c
+++ i2c.c
@@ -1,138 +1,58 @@
-#include <avr/io.h>  
-#include "i2c.h"  
- 
-#define I2CDir DDRC  
-#define I2COut PORTC  
-#define I2CIn  PINDC 
-//  
-#define SDA 5  
-#define SCL 6  
+#include <avr/io.h>
+#include "i2c.h"
 
-#define SET_SDA I2COut |= (1 << SDA)  
-#define CLR_SDA I2COut &= ~(1 << SDA)  
+void I2C_SetBitrate(uint16_t bitrateKHz)
+{
+	uint8_t bitrate_div;
 
-#define SET_SCL I2COut |= (1 << SCL)  
-#define CLR_SCL I2COut &= ~(1 << SCL)  
+	bitrate_div = (( F_CPU / 1000l ) / bitrateKHz);
+	if (bitrate_div >= 16) bitrate_div = (bitrate_div-16)/2;
 
-#define SDA_OUT I2CDir |= (1 << SDA)  
-#define SDA_IN  I2CDir &= ~(1 << SDA)  
+	TWBR = bitrate_div;
+}
 
-#define SCL_OUT I2CDir |= (1 << SCL)  
-#define SCL_IN  I2CDir &= ~(1 << SDA)  
+void TWI_start(void)
+{
+	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTA);
+	while (!(TWCR&(1<<TWINT)));
+}
 
-#define GET_SDA (I2CIn & (1 << SDA))  
+void TWI_stop(void)
+{
+	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO);
+	while ( (TWCR&(1<<TWSTO)));
+}
 
-#define ACK 1  
-#define NACK 0  
+void TWI_write(uint8_t bajt)
+{
+	TWDR = bajt;
+	TWCR = (1<<TWINT)|(1<<TWEN);
+	while ( !(TWCR&(1<<TWINT)));
+}
 
-void delay(void)  
-{  
-	asm("nop");  
-	asm("nop");  
-	asm("nop");  
-	asm("nop");  
-}  
+uint8_t TWI_read(uint8_t ack)
+{
+	TWCR = (1<<TWINT)|(ack<<TWEA)|(1<<TWEN);
+	while ( !(TWCR & (1<<TWINT)));
+	return TWDR;
+}
 
-// funkcja generujaca sygna³ start  
-void I2C_Start(void)  
-{  
-	SDA_OUT; //  
-	SCL_OUT; // ustawienie linii SDA i SCL w tryb wyjœciowy  
-	SET_SDA; //  
-	SET_SCL; // ustawienie na liniach SDA i SCL stanu wysokiego  
-	delay(); // opóŸnienie  
-	CLR_SDA;  
-	delay();  
-	CLR_SCL;  
-}  
+void TWI_write_buf( uint8_t SLA, uint8_t adr, uint8_t len, uint8_t *buf )
+{
+	TWI_start();
+	TWI_write(SLA);
+	TWI_write(adr);
+	while (len--) TWI_write(*buf++);
+	TWI_stop();
+}
 
-// funkcja generujaca sygna³ stop  
-void I2C_Stop(void)  
-{  
-	CLR_SDA;  
-	delay();  
-	SET_SCL;  
-	delay();  
-	SET_SDA;  
-	delay();  
-}  
-  
-// funkcja wysy³aj¹ca bajt na szynê I2C  
-void I2C_Write(unsigned char x)  
-{  
-	unsigned char count = 9;  
-	do {  
-		CLR_SCL;  
-		if(x & 0x80)
-		{  
-			SET_SDA;  
-		}
-		else
-		{  
-			CLR_SDA;  
-		}  
-		x <<= 1;  
-		delay();  
-		SET_SCL;  
-		delay();  
-	} while(--count);  
-	CLR_SCL;  
-}  
-  
-// funkcja odczytujaca bajt z szyny I2C  
-unsigned char I2C_Read(unsigned char ack)  
-{  
-	unsigned char count = 8, temp = 0;  
-	SET_SDA;  
-	SDA_IN;  
-	do {  
-		delay();  
-		SET_SCL;  
-		delay();  
-		temp <<= 1;  
-		if(GET_SDA)
-		{  
-			temp++;  
-		}  
-		CLR_SCL;  
-	} while(--count);  
-	if(ack)
-	{  
-		SET_SDA;  
-	}	  
-	delay();  
-	SET_SCL;  
-	delay();  
-	CLR_SCL;  
-	return (temp);  
-}  
-  
-//odczyt z I2C spod urz¹dzenia o adresie 'SLA' z podadresu 'adr' iloœci  
-//bajtów 'len' i zachowanie w 'buf'  
-void I2C_readBuf(uint8_t sla, uint8_t adr, uint8_t len, uint8_t *buf)  
-{  
-	I2C_Start();  
-	I2C_Write(sla);  
-	I2C_Write(adr);  
-	I2C_Start();  
-	I2C_Write(sla + 1);  
-	while(len--)
-	{  
-		*buf++ = I2C_Read(len ? ACK : NACK);  
-	}  
-	I2C_Stop();  
-}  
-  
-//zapis na I2C do urz¹dzenia o adresie 'SLA' do podadresu 'adr' iloœci   
-//bajtów 'len' spod podrêcznego 'buf'  
-void I2C_writeBuf(uint8_t sla, uint8_t adr, uint8_t len, uint8_t *buf)  
-{  
-	I2C_Start();  
-	I2C_Write(sla); //wysy³ka adresu slave-a  
-	I2C_Write(adr); //wysy³ka adresu rejestru slave-a (SUB)  
-	while(len--)
-	{  
-		I2C_Write(*buf++);  //wysy³ka danych  
-	}  
-	I2C_Stop(); //koniec transmisji  
-}  
+void TWI_read_buf(uint8_t SLA, uint8_t adr, uint8_t len, uint8_t *buf)
+{
+	TWI_start();
+	TWI_write(SLA);
+	TWI_write(adr);
+	TWI_start();
+	TWI_write(SLA + 1);
+	while (len--) *buf++ = TWI_read( len ? ACK : NACK );
+	TWI_stop();
+}
---
+++ lsm303d.h
@@ -0,0 +1,86 @@
+/*
+ * lsm303d.h
+ *
+ * Created: 03.01.2017 17:49:33
+ *  Author: Magnus
+ */ 
+
+
+#ifndef LSM303D_H_
+#define LSM303D_H_
+
+#define LSM303D_ADDR	0x3A
+#define ADDR_WRITE		0x3A
+#define ADDR_READ		0x3B
+
+#define TEMP_OUT_L		0x05
+#define TEMP_OUT_H		0x06
+
+#define STATUS_M		0x07
+#define OUT_X_L_M       0x08            
+#define OUT_X_H_M       0x09
+#define OUT_Y_L_M       0x0A
+#define OUT_Y_H_M       0x0B
+#define OUT_Z_L_M       0x0C
+#define OUT_Z_H_M       0x0D
+
+#define WHO_I_AM		0x0F
+#define INT_CTRL_M		0x12
+#define INT_SRC_M		0x13
+#define INT_THS_L_M		0x14
+#define INT_THS_H_M		0x15
+
+#define OFFSET_X_L_M    0x16
+#define OFFSET_X_H_M    0x17
+#define OFFSET_Y_L_M    0x18
+#define OFFSET_Y_H_M    0x19
+#define OFFSET_Z_L_M    0x1A
+#define OFFSET_Z_H_M    0x1B
+
+#define REFERENCE_X		0x1C
+#define REFERENCE_Y		0x1D
+#define REFERENCE_Z		0x1E
+
+#define CTRL0           0x1F
+#define CTRL1           0x20
+#define CTRL2           0x21
+#define CTRL3           0x22
+#define CTRL4           0x23
+#define CTRL5           0x24
+#define CTRL6           0x25
+#define CTRL7           0x26
+
+#define STATUS_A        0x27
+#define OUT_X_L_A       0x28
+#define OUT_X_H_A       0x29
+#define OUT_Y_L_A       0x2A
+#define OUT_Y_H_A       0x2B
+#define OUT_Z_L_A       0x2C
+#define OUT_Z_H_A       0x2D
+
+#define FIFO_CTRL       0x2E            // Rejestr kontrolny bufora FIFO, typu read/write.
+#define FIFO_STAT       0x2F            // Status rejestru bufora FIFO, typu read.
+
+#define WriteCTRL0      0x04            // High-pass filter enabled for click function.
+#define WriteCTRL1      0x27            // Makro wybudzaj¹ce akcelerometr, oraz ustawiaj¹ce odswiezanie na 6.25Hz
+#define WriteCTRL4      0x08            // Accelerometer data-ready signal on INT2.
+#define WriteCTRL5      0               // Makro ustawiajace czêstotliwosc samplowania magnetometru na 3.125Hz
+#define WriteCTRL7      0xA0            // High-pass filter mode selection: normal mode & Magnetic sensor mode selection: Continuous-conversion mode
+
+void LSM303D_Init_I2C(void)
+{
+	TWI_start();
+	TWI_write(ADDR_WRITE);
+	TWI_write(CTRL1);
+	TWI_write(WriteCTRL1);
+	TWI_stop();
+		
+	TWI_start();
+	TWI_write(ADDR_WRITE);
+	TWI_write(CTRL7);
+	TWI_write(WriteCTRL7);
+	TWI_stop();
+	
+}
+
+#endif /* LSM303D_H_ */
