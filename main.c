ce4ad1cc46ce1bd73130016f962193679f8e25cd
MagnusArias <magnusarias.95@gmail.com>
2017-01-10 15:04:41 +0100

    dodano sturktury do obslugi danych pochadzcych z 3DS oraz do wysylania danych. Dodakotow, funkcje do osblugi sturktur

--- main.c
+++ main.c
@@ -4,37 +4,85 @@
 #include "i2c.h"
 #include "lsm303d.h"
 
+void sendStruct(struct sDataPacket *src);
+void recieveStruct(struct sControlPacket *dst, uint8_t size);
+
+struct sDataPacket
+{
+	uint16_t _accX;
+	uint16_t _accY;
+	uint16_t _accZ;
+	uint16_t _temp;
+	uint16_t _magX;
+	uint16_t _magY;
+	uint16_t _magZ;
+	const unsigned char __CR	= 0x0D;	// stala wymagana do wyslania do ESP
+	const unsigned char __LF	= 0x0A; // stala wymagana do wyslania do ESP
+	const unsigned char __0		= 0;	// stala wymagana do wyslania do ESP
+} sData;
+
+struct sControlPacket
+{
+	int16_t _xAxis;
+	int16_t _yAxis;
+	int16_t _buttons;
+} sControl;
+
+uint16_t readLSM303D(uint8_t addr) 
+{
+	uint8_t bufor[2];
+	TWI_read_buf(LSM303D_ADDR, addr, 8, bufor);
+	return (((bufor[1]) << 8) | (bufor[0]));
+}
+
 int main(void)
 {
 	LCD_Initalize();
 	I2C_SetBitrate(100);
 	LSM303D_Init_I2C();
-	
-	int bufor[2];
-	int wynikX, wynikY, wynikZ;
-
 
 	while(1)
 	{
-		TWI_read_buf( LSM303D_ADDR, OUT_X_H_A , 8, bufor );
-		wynikX = (( (bufor[1])<<8) | (bufor[0]));
-		
-		TWI_read_buf( LSM303D_ADDR, OUT_Y_H_A , 8, bufor );
-		wynikY = (( (bufor[1])<<8) | (bufor[0]));
+		receiveStruct(sControl);	// odbieranie struktury od 3DS
+		//sControl._yAxis	- wyjscie na tylne silniki
+		//sControl._xAxis	- wyjscie an serwo
+		//sControl._buttons	- info o przyciskach, idize na diody itd
 
-		TWI_read_buf( LSM303D_ADDR, OUT_Z_H_A , 8, bufor );
-		wynikZ = (( (bufor[1])<<8) | (bufor[0]));
+
+		sData._accX = readLSM303D(OUT_X_H_A);
+		sData._accY = readLSM303D(OUT_Y_H_A);
+		sData._accZ = readLSM303D(OUT_Z_H_A);
+		sData._temp = readLSM303D(TEMP_OUT_H);
+		sData._magX = readLSM303D(OUT_X_H_M);
+		sData._magY = readLSM303D(OUT_Y_H_M);
+		sData._magZ = readLSM303D(OUT_Z_H_M);
 		
-		LCD_GoTo(0,0);	LCD_WriteText("X:   Y:   Z:");
-		
-		LCD_GoTo(0,1);	LCD_WriteInteger(wynikX);
-		
-		LCD_GoTo(5,1);	LCD_WriteInteger(wynikY);
-		
-		LCD_GoTo(10,1);	LCD_WriteInteger(wynikZ);
-		
-		_delay_ms(50);
-		LCD_Clear();
+		sendStruct(sData);			// wysylanie struktury do 3DSa
 	} 
-	
+}
+
+
+void sendStruct(struct sDataPacket *src)
+{
+	char *srcPointer = src;
+
+	char size = sizeof(*src);
+
+	char i;
+	for (i = 0; i < size; i++)
+	{
+		SerialTransmitChar(*srcPointer++);
+	}
+}
+
+void recieveStruct(struct sControlPacket *dst, uint8_t size)
+{
+	char *dstPointer = dst;
+
+	char i;
+	for (i = 0; i < size; i++)
+	{
+		*dstPointer = SerialReceiveChar();
+		dstPointer++;
+	}
 }
