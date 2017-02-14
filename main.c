13b7dc5cc1501adc9beb29da7ace9a437c852ca5
MagnusArias <magnusarias.95@gmail.com>
2017-02-14 21:32:19 +0100

    Dodano obsluge silnikow oraz serwomechanizmu, z pelna kontrola

--- main.c
+++ main.c
@@ -1,9 +1,115 @@
 #include <avr/io.h>
 #include <util/delay.h>
-#include "HD44780.h"
+
 #include "i2c.h"
 #include "uart.h"
 #include "lsm303d.h"
+
+#define L_IN_1		(1<<PC3)
+#define L_IN_2		(1<<PC2)
+#define PWM_L		(1<<PD7)	// OC2A
+
+#define P_IN_1		(1<<PA2)
+#define P_IN_2		(1<<PA3)
+#define PWM_P		(1<<PB4)	// OC0B
+
+#define PWM_SERWO	(1<<PD5)	// OC1A
+
+void initMotors(void)
+{
+	/*
+		Initalizes rear motors, pins connected to H-bridge
+		2 pins for control and 1 for speed (PWM)
+	*/
+	
+	DDRA |= (P_IN_1 | P_IN_2);
+	DDRB |= (PWM_P);
+	DDRC |= (L_IN_1 | L_IN_2);
+	DDRD |= (PWM_L);
+	
+	TCCR0A |= (1 << COM0B1 | 1 << WGM01 | 1<< WGM00);
+	TCCR0B |= (1 << CS00);
+	
+	TCCR2A |= (1 << COM2A1 | 1 << WGM21 | 1 << WGM20);
+	TCCR2B |= (1 << CS20);
+	
+	OCR0B = 0;
+	OCR2A = 0;
+	
+}
+
+void runMotors(char dir, char power)
+{
+	/*
+		Driving the motors
+		"dir" means which direction (forward od backward)
+		"power" means power, obviously, values betwen 0-255
+	*/
+	
+	switch (dir){
+		// FORWARD
+		case 1:
+			PORTC &= ~(L_IN_2);
+			PORTC |= (L_IN_1);
+			
+			PORTA &= ~(P_IN_2);
+			PORTA |= (P_IN_1);
+			
+			OCR0B = power;
+			OCR2A = power;
+			break;
+		
+		// BACKWARD
+		case 2:
+			PORTC &= ~(L_IN_1);
+			PORTC |= (L_IN_2);
+			
+			PORTA &= ~(P_IN_1);
+			PORTA |= (P_IN_2);
+		
+					
+			OCR0B = power;
+			OCR2A = power;
+			break;
+		
+		// STOP
+		case 0:
+			PORTC &= ~(L_IN_1 | L_IN_2);
+			PORTA &= ~(P_IN_1 | P_IN_2);
+			
+			OCR0B = 0;
+			OCR2A = 0;
+			break;
+			
+		// also STOP
+		default:
+			PORTC &= ~(L_IN_1 | L_IN_2);
+			PORTA &= ~(P_IN_1 | P_IN_2);
+			
+			OCR0B = 0;
+			OCR2A = 0;
+			break;
+	}
+}
+
+void initServo(void)
+{
+	/*
+		Initalizes servomotor, only one pin needed to control (PWM)
+	*/
+	DDRD |= (PWM_SERWO);
+	TCCR1A |= (1 <<COM1A1 | 1 << WGM11);
+	TCCR1B |= (1 << WGM13 | 1<< CS11 | 1<< CS10); // prescaler /64 - timer period euqals to 3.74us
+	ICR1 = 5759; // max value
+}
+
+void moveServo(int value){
+	/*
+		Moves the servo left or right
+		As i remember, max-left position is 200, max-right position was 300
+	*/
+	OCR1A = value;
+}
 
 void readSensors(struct sDataPacket s)
 {
@@ -43,6 +149,9 @@ void removeIPD(unsigned char* src, unsigned char* dest)
 
 int main(void)
 {	
+
+	initMotors();
+	initServo();
 	unsigned char MUX[]		= "AT+CIPMUX=1";
 	unsigned char MODE[]	= "AT+CWMODE=1";
 	unsigned char CWJAP[]	= "AT+CWJAP=\"Shin\",\"haslo12345\"";
@@ -54,9 +163,7 @@ int main(void)
 	
 	unsigned char receivedData[17];
 	unsigned char controlPacket[sizeof(sControl)];
-	LCD_Initalize();
-	_delay_ms(1000);
-	LCD_Clear();
+
 	
 	I2C_SetBitrate(100);
 	LSM303D_Init_I2C();
@@ -80,6 +187,8 @@ int main(void)
 		readSensors(sData);
 		SerialTransmit(SEND);
 		SendStruct(&sData);
+		
+		runMotors(1,250):
 		
 	} 
 }
